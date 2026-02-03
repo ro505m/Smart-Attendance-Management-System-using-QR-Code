abf298423306cb6478a26e0d200f0fec765d3d25
rasool hussein <rasool85hussein@gmail.com>
2026-02-03 17:50:50 +0300

    real database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to mongodb...");
     } catch (error) {
         console.log("Connection failed to mongodb!", error);
