15c52fa4897ee9858eb9b4fa164f5a97d047172b
rasool hussein <rasool85hussein@gmail.com>
2026-02-03 20:43:17 +0300

    time out 5000

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,{
+            serverSelectionTimeoutMS: 5000
+        });
         console.log("Connected to mongodb...");
     } catch (error) {
         console.log("Connection failed to mongodb!", error);
